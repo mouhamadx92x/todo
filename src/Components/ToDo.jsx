da70d6b0272642f1d5ffa622d1bc2983dd10664a
mouhamadx92x <mouhamadx92x@gmail.com>
2023-01-26 15:30:57 +0100

    update the filter method

--- src/Components/ToDo.jsx
+++ src/Components/ToDo.jsx
@@ -1,44 +1,41 @@
-import React from 'react';
-import { v4 as uuidv4 } from 'uuid';
-import { useState } from 'react';
-
+import React from "react";
+import { v4 as uuidv4 } from "uuid";
+import { useState } from "react";
 
 const Todo = () => {
+  const [todos, setTodos] = useState([]);
+  const [value, setValue] = useState("");
+
+  const addHandler = () => {
+    setTodos([...todos, { name: value, id: uuidv4(), isDone: false }]);
+  };
+
+  const delethandler = (id) => {
+    const newTodo = todos.filter((element) => {
+      return element.id != id;
+    });
     
-    const [todos, setTodos] = useState([]);
-    const [value, setValue] = useState("");
-
-
-const addHandler  = ()=>{
-setTodos([...todos,{name:value,id:uuidv4(),isDone:false}])
-}
-const delethandler = (e)=>{
-  
-const newTodo = todos.filter((element)=>{
-  console.log("text" ,e.target.previousElementSibling.textContent)
-  console.log("element", element.name === e.target.previousElementSibling.textContent )
-  console.log(element.name)
-return element.name != e.target.previousElementSibling.textContent;
-
-  })
-  
-  console.log(newTodo)
-  setTodos([...newTodo])
-}
+    setTodos([...newTodo]);
+  };
 
   return (
     <div>
-<input type="text" onChange={(e)=>{setValue(e.target.value)}}/>
-<button onClick={addHandler}>Add</button>
+      <input
+        type="text"
+        onChange={(e) => {
+          setValue(e.target.value);
+        }}
+      />
+      <button onClick={addHandler}>Add</button>
 
-{todos.map((todo)=>(
-  <div key={todo.id}>
-    <p>{todo.name}</p>
-    <button onClick={delethandler}>x</button>
-  </div>
-))}
+      {todos.map((todo) => (
+        <div key={todo.id}>
+          <p>{todo.name}</p>
+          <button onClick={()=>delethandler(todo.id)}>x</button>
+        </div>
+      ))}
     </div>
-  )
-}
+  );
+};
 
-export default Todo
+export default Todo;
